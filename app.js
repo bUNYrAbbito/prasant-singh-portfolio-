9bed8aba283ca9a9b66b251f2d9787c2a0a9d2f9
bUNYrAbbito <singhbunny116@gmail.com>
2024-09-21 10:52:51 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define a simple route
+app.get('/', (req, res) => {
+    res.render('index');
+});
+app.get('/home', (req, res) => {
+    res.render('index');
+});
+
+app.get('/about',(req,res) => {
+    res.render('about');
+})
+app.get('/contact',(req,res) => {
+    res.render('contact');
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
